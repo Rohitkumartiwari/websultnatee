06409a6a05d31e2cf32d96139dfda47011bc08cb
Rohitkumartiwari <sonutiwari.july@gmail.com>
2024-06-18 21:53:37 +0530

    chnages done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import LiveDeops from "./components/LiveDeops";
+import UpperSidebar from "./components/UpperSidebar";
+import HeaderBanner from "./components/HeaderBanner";
+import ChatSection from "./components/ChatSection";
+import FeaturedBoxes from "./components/FeaturedBoxes";
+import LowerSidebar from "./components/LowerSidebar";
+import LatestBattle from "./components/LatestBattle";
+import FreshDelivery from "./components/FreshDelivery";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header/>
+      <div className="  px-3 py-3 chat-grid">
+        <div className="">
+          <div className="row">
+            <div className="col-md-1 px-2 upper-sidebar">
+              <UpperSidebar />
+            </div>
+            <div className="col-md-11">
+              <p className="paragraph d-flex align-items-center text-white drops">
+                <span class="blink-text me-1"></span>Live Drops
+              </p>
+              <LiveDeops />
+              <HeaderBanner />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-1 px-2 upper-sidebar">
+              <LowerSidebar />
+            </div>
+            <div className="col-md-11">
+              <FeaturedBoxes />
+            </div>
+          </div>
+         <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-11">
+          <LatestBattle/>
+          <FreshDelivery/>
+          <Footer />
+          </div>
+         </div>
+          
+        </div>
+
+        <div className=" px-2 chat-section">
+          <ChatSection />
+        </div>
+      </div>
     </div>
   );
 }
